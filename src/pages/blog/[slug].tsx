2452eeef462c8d83105e1376f81eef7dbe6ca07e
Caleb Lovell <caleblovell1@gmail.com>
2021-08-10 02:51:56 -0600

    mdx working without styling

---
+++ src/pages/blog/[slug].tsx
@@ -0,0 +1,57 @@
+import { getAllPostsMeta, getPostBySlug } from '@utils/mdx';
+import { MdxComponents } from '@components/MdxComponents';
+import { getMDXComponent } from 'mdx-bundler/client';
+import { GetStaticProps } from 'next';
+import Image from 'next/image';
+import React from 'react';
+
+export const getStaticPaths = () => {
+	const posts = getAllPostsMeta();
+	const paths = posts.map(({ slug }) => ({ params: { slug } }));
+
+	return {
+		paths: paths,
+		// Return 404 page if path is not returned by getStaticPaths
+		fallback: false,
+	};
+};
+
+export const getStaticProps: GetStaticProps = async context => {
+	const slug = context.params?.slug as string;
+	const post = await getPostBySlug(slug);
+
+	return { props: post };
+};
+
+export default function PostPage({ meta, code }) {
+	// This is a bit weird, but this is how mdx-bundler recommends it.
+	const Component = React.useMemo(() => getMDXComponent(code), [code]);
+
+	return (
+		<div className='container max-w-3xl px-4 mx-auto mt-36'>
+			<h1 className='text-2xl font-bold md:text-4xl'>{meta.title}</h1>
+
+			<div className='flex items-center mt-4 space-x-2 text-gray-500'>
+				<Image src='/avatar.jpg' height={24} width={24} className='rounded-full' />
+
+				<div>Caleb Lovell</div>
+
+				<div className='text-gray-300'>&middot;</div>
+
+				<div>{'a date goes here'}</div>
+			</div>
+
+			{meta.image ? (
+				<div className='mt-10 overflow-hidden rounded-2xl text-[0px]'>
+					<Image src={`/${meta.image}`} width={1920} height={960} />
+				</div>
+			) : null}
+
+			<div className='mt-10 text-gray-900'>
+				<Component components={MdxComponents} />
+			</div>
+
+			<div className='flex justify-center mt-16 space-x-8'>{meta.source ? <button>View Source Code</button> : null}</div>
+		</div>
+	);
+}
---
+++ src/utils/mdx.ts
@@ -0,0 +1,54 @@
+import fs from 'fs';
+import glob from 'glob';
+import matter from 'gray-matter';
+import { bundleMDX } from 'mdx-bundler';
+import path from 'path';
+
+const ROOT_PATH = process.cwd();
+const POSTS_PATH = path.join(ROOT_PATH, 'posts');
+
+export const getAllPostsMeta = () => {
+	const PATH = path.join(POSTS_PATH);
+
+	// Get all file paths in the posts folder (that end with .mdx)
+	const paths = glob.sync(`${PATH}/**/*.mdx`);
+
+	return (
+		paths
+			.map(filePath => {
+				// Get the content of the file
+				const source = fs.readFileSync(path.join(filePath), 'utf8');
+
+				// Get the file name without .mdx
+				const slug = path.basename(filePath).replace('.mdx', '');
+				// Use gray-matter to extract the post meta from post content
+				const data = matter(source).data;
+
+				return {
+					...data,
+					slug,
+				};
+			})
+
+			// Sort posts by published date
+			.sort((a, b) => Number(new Date(b.publishedAt)) - Number(new Date(a.publishedAt)))
+	);
+};
+
+// Get content of specific post
+export const getPostBySlug = async (slug: string) => {
+	// Get the content of the file
+	const source = fs.readFileSync(path.join(POSTS_PATH, `${slug}.mdx`), 'utf8');
+
+	const { code, frontmatter } = await bundleMDX(source);
+
+	const meta = {
+		...frontmatter,
+		slug,
+	};
+
+	return {
+		meta,
+		code,
+	};
+};
