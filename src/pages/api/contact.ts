8af0f8fa9984563bda3ea71490ccbc5155f81a86
CalebLovell <caleblovell1@gmail.com>
2022-03-01 15:57:51 -0500

    replace gmail smtp with nodemailer

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,33 +1,37 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { MailOptions } from 'nodemailer-mailgun-transport';
+import { Transporter } from 'nodemailer';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-	require(`dotenv`).config();
-	const nodemailer = require(`nodemailer`);
-	const transporter = nodemailer.createTransport({
-		port: 465,
-		host: `smtp.gmail.com`,
-		auth: {
-			user: `clovellemailer@gmail.com`,
-			pass: process.env.EMAIL_PASSWORD,
-		},
-		secure: true,
-	});
-
-	const email = {
-		from: `clovellemailer@gmail.com`,
-		to: `caleblovell1@gmail.com`,
-		subject: `Message From ${req.body.name}`,
-		text: `Prompt: ${req.body.prompt} Message: ${req.body.message} | Sent from: ${req.body.email}`,
-		html: `<div><p>From: ${req.body.name}</p><p>Email: ${req.body.email}</p><p><b>Prompt: ${req.body.prompt}</b></p><p><p>${req.body.message}</p>Reply to: ${req.body.email}</p></div>`,
-	};
-
 	try {
-		transporter.sendMail(email, (err, info) => {
-			if (err) console.log(err);
-			else res.status(200).json({ message: info });
+		const nodemailer = require(`nodemailer`);
+		const mg = require(`nodemailer-mailgun-transport`);
+
+		const mailgunAuth = {
+			auth: {
+				api_key: process.env.MAILGUN_API_KEY,
+				domain: process.env.MAILGUN_DOMAIN,
+			},
+		};
+
+		const smtpTransport: Transporter = nodemailer.createTransport(mg(mailgunAuth));
+
+		const email: MailOptions = {
+			from: `caleblovell1@gmail.com`,
+			to: `caleblovell1@gmail.com`,
+			subject: `Message From ${req.body.name}`,
+			text: `Prompt: ${req.body.prompt} Message: ${req.body.message} | Sent from: ${req.body.email}`,
+			html: `<div><p>From: ${req.body.name}</p><p>Email: ${req.body.email}</p><p><b>Prompt: ${req.body.prompt}</b></p><p><p>${req.body.message}</p>Reply to: ${req.body.email}</p></div>`,
+		};
+
+		smtpTransport.sendMail(email, (error, response) => {
+			if (error) {
+				console.log(error.message);
+				res.status(500).send({ error });
+			} else res.status(200).send({ message: response });
 		});
 	} catch (err) {
-		res.status(500).json({ error: err.message });
+		res.status(500).send({ error: err });
 	}
 }
